702903b998e02923cc821e8a06b04681ae983dcb
fredghostkyle <fredghostkyle@yahoo.com>
2016-06-08 14:15:47 -0400

    dummy numbers

--- src/edu/Andrew/APCS/Adventure/Testers/MonsterTester.java
+++ src/edu/Andrew/APCS/Adventure/Testers/MonsterTester.java
@@ -10,7 +10,7 @@ public class MonsterTester
     public static void main(String args[]) {
         Monster skrub = new Monster("skrub", 20, 10, 1, 10, 25, 5, 25, true, null);
         Monster skrublord = new Monster("skrublord", 25, 20, 10, 50, 35, 10, 45, true, null);
-        Boss wyvern = new Boss("Wyvern", 400, 45, 2000, true, null, 500, 5000);
+        Boss wyvern = new Boss("Wyvern", 400, 1, 50, 50, 1000, 45, 2000, true, null, 500, 5000);
 
         System.out.println(skrub.toString());
         System.out.println(skrublord.toString());
